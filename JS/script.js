be735de3aa9fad5daef0a187a4165e6af8b9a8ae
Shivaraj2003 <shivarajshetty635@gmail.com>
2024-02-21 20:56:01 +0530

    Added navbar and item list. Implemented search functionality

---
+++ JS/script.js
@@ -0,0 +1,158 @@
+const displayContainer = document.getElementById("productContainer");
+var products ;
+
+var cart ={} ;
+
+async function loadDataAndDisplay() {
+  try {
+    const response = await fetch("../data.json");
+    const data = await response.json();
+    products = data.shoes;
+    displayData(products);
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error);
+  }
+}
+
+function displayData(items) {
+
+  displayContainer.innerHTML = "";
+  for (let i = 0; i < items.length; i++) {
+    const product = items[i];
+    displayContainer.innerHTML += `
+      <div class="card col-md-4 p-2 ">
+        <div class="card-body">
+          <h3 class="card-title">${product.title}</h3>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+          <img src="${product.img}" class="card-img-top" alt="..." height="350" width="350" >
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <button class="cart" id=${product.id}>Cart</button>
+          <a href="#" class="btn btn-primary">Buy</a>
+        </div>
+      </div>
+    `;
+  }
+
+
+
+const cartItems = document.querySelectorAll('.cart');
+
+  for (let i = 0; i < cartItems.length; i++) {
+  const button = cartItems[i];
+
+  button.addEventListener('click', () => {
+  const productId = button.id;
+      
+    if (cart.hasOwnProperty(productId)) {
+      cart[productId] += 1;
+      button.textContent = "added";
+      button.style.color = 'green';
+      
+      console.log('Already have product:',productId,cart[productId]);
+    } else { 
+      cart[productId] = 1;
+      console.log('Added product:', cart[productId]);
+      button.textContent = "remove";
+      button.style.color = 'red'; //this is just for testing
+    }
+  });
+}
+
+
+}
+
+loadDataAndDisplay();
+function filterProducts(searchInput) {
+  
+let filteredProducts = [];
+  for(let i=0 ; i<products.length;i++)
+  {
+    if(products[i].title.toLowerCase().includes(searchInput.toLowerCase()))
+    {
+      filteredProducts.push(products[i]);
+    }
+  }
+
+  if(filteredProducts.length!==0)
+     displayData(filteredProducts);
+  else{
+    displayContainer.innerHTML = `<h1>Oops we dont have ${searchInput} right now</h1>`
+  }
+}
+
+document.querySelector("#search").addEventListener("click", () => {
+  const searchInput = document.querySelector("#searchInp").value;
+  filterProducts(searchInput);
+});
+
+
+document.querySelector("#my_cart").addEventListener("click", () => {
+
+
+ 
+getCart();
+  
+  
+});
+
+
+function getCart(){
+  let cartStore = []
+  for(let id in cart)
+  {
+    if(cart.hasOwnProperty(id))
+    {
+      cartStore.push(products[id-1]);
+    }
+  }
+  console.log(cartStore);
+  
+  displayCartData(cartStore);
+
+}
+
+function displayCartData(items) {
+
+  displayContainer.innerHTML = "";
+  for (let i = 0; i < items.length; i++) {
+    const product = items[i];
+    displayContainer.innerHTML += `
+      <div class="card col-md-4 p-2">
+        <div class="card-body">
+          <h3 class="card-title">${product.title}</h3>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+          <img src="${product.img}" class="card-img-top" alt="..." height="350" width="350">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <button class="cartRemove" id=${product.id}>Remove</button>
+          <a href="#" class="btn btn-primary">Buy</a>
+        </div>
+      </div>
+    `;
+  }
+
+
+  const cartRemoveItems = document.querySelectorAll('.cartRemove');
+
+  for (let i = 0; i < cartRemoveItems.length; i++) {
+  const button = cartRemoveItems[i];
+
+  button.addEventListener('click', () => {
+  const productId = button.id;
+
+    if (cart.hasOwnProperty(productId)) {
+      delete cart[productId];
+      getCart();
+    } else {
+      cart[productId] = 1;
+      console.log('Added product:', cart[productId]);
+    }
+  });
+}
+
+if(cartRemoveItems.length==0)
+{
+  displayContainer.innerHTML = `Add something to cart;
+  <a href=index.html>Go to home page</a>`
+}
+}
+
