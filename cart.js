94b585bfb1506863ae13a5ea6e1a303cf7e9bf3e
Shivaraj2003 <shivarajshetty635@gmail.com>
2024-02-29 14:03:06 +0530

    Fixed responsiveness of cart page for smaller screens

--- cart.js
+++ cart.js
@@ -1,27 +1,27 @@
-var cartContainer = document.querySelector("#cartContainer");
-var totalPriceSpan = document.getElementById("totalPrice");
+var cartContainer = document.querySelector('#cartContainer');
+var totalPriceSpan = document.getElementById('totalPrice');
 let totalPrice = 0;
-cartContainer.innerHTML = "";
-let cartData = JSON.parse(localStorage.getItem("cart")) || [];
+cartContainer.innerHTML = '';
+let cartData = JSON.parse(localStorage.getItem('cart')) || [];
 
 // console.log(typeof cartData);
 function remove(j) {
-    const newCartData = cartData.filter((object) => {
-        return object.id !== j;
-    });
-    localStorage.setItem("cart", JSON.stringify(newCartData));
-    location.reload();
+  const newCartData = cartData.filter((object) => {
+    return object.id !== j;
+  });
+  localStorage.setItem('cart', JSON.stringify(newCartData));
+  location.reload();
 }
 
 if (cartData.length === 0) {
-    cartContainer.innerHTML = `<h3 class="p-3 m-3 fw-bold fs-7 text-center">Not added any items to cart</h3>
+  cartContainer.innerHTML = `<h3 class="p-3 m-3 fw-bold fs-7 text-center">Not added any items to cart</h3>
       <div class="text-center">
         <a href="index.html"><button type="button" class="btn btn-primary mb-3 fw-bold">Go to Home Page</button></a>
       </div>`;
 
-    document.querySelector("#pricing").innerHTML = "";
+  document.querySelector('#pricing').innerHTML = '';
 } else {
-    cartContainer.innerHTML = `<div class="card-body fw-bold p-4 border border-2 border-black " id="productRow">
+  cartContainer.innerHTML = `<div class="card-body fw-bold p-4 border border-2 border-black " id="productRow">
     <div class="row d-flex justify-content-between align-items-center bg-light">
     <div class="col-md-2 col-lg-2 col-xl-2">Product Detail</div>
     <div class="col-md-3 col-lg-3 col-xl-3">Quantity</div>
@@ -30,8 +30,8 @@ if (cartData.length === 0) {
     <div class="col-md-1 col-lg-1 col-xl-1 text-end"></div>
   </div>
   </div>`;
-    for (let i = 0; i < cartData.length; i++) {
-        cartContainer.innerHTML += `<div class="card-body p-4 border border-2 border-black ">
+  for (let i = 0; i < cartData.length; i++) {
+    cartContainer.innerHTML += `<div class="card-body p-4 border border-2 border-black " >
         <div class="row d-flex justify-content-between align-items-center">
           <div class="col-md-2 col-lg-2 col-xl-2">
             <img src=${cartData[i].img} class="img-fluid rounded-3 " alt="Shoez" style="height: 20%; width:10%/>
@@ -39,11 +39,11 @@ if (cartData.length === 0) {
 
           <div class="col-md-3 col-lg-3 col-xl-3">
             <p class="lead fw-normal fw-bold mb-2">${cartData[i].title}</p>
-            <p>
-              <span class="text-muted">Size: ${cartData[i].size}</span>
+            <span>
+              <span ">Size:${cartData[i].size}</span>
               <br>
-              <span class="text-muted">Color:</span>${cartData[i].color}
-            </p>
+              <span class="fw-bold">${cartData[i].color}</span>
+            </span>
           </div>
 
           <div class="col-md-3 col-lg-2 col-xl-2 mb-2 d-flex">
@@ -58,12 +58,15 @@ if (cartData.length === 0) {
             </button>
           </div>
 
-          <div class="col-md-3 col-lg-2 col-xl-2">
+          <div class="col-md-3 col-lg-2 col-xl-2 " id='mg'>
+            <span id='media1' class="fw-bold">Unit Price: </span>
+
             <h5 class="mb-0">&#8377;${cartData[i].price}</h5>
           </div>
 
-           <div class="col-md-3 col-lg-2 col-xl-2">
-            <h5>&#8377;<span id = 'total${i}' class="mb-0">${cartData[i].price}</span></h5>
+           <div class="col-md-3 col-lg-2 col-xl-2 ">
+           <span id='media1' class="fw-bold">Total Price: </span>
+            <h5 >&#8377;<span id = 'total${i}' class="mb-0">${cartData[i].price}</span></h5>
           </div>
 
           <div id="removeButton${i}" class="col-md-1 col-lg-1 col-xl-1 text-end remove")">
@@ -72,67 +75,64 @@ if (cartData.length === 0) {
         </div>
       </div>`;
 
-        update(i);
+    update(i);
 
-        totalPrice += cartData[i].price;
-    }
+    totalPrice += cartData[i].price;
+  }
 
-    totalPriceSpan.innerText = totalPrice;
-    sessionStorage.setItem('cartAmount',totalPrice);
-
+  totalPriceSpan.innerText = totalPrice;
+  sessionStorage.setItem('cartAmount', totalPrice);
 }
 
 const removeButton = document.querySelectorAll(`.remove`);
 
 for (let i = 0; i < cartData.length; i++) {
-    const btnRemove = removeButton[i];
-    btnRemove.addEventListener("click", () => {
-        remove(cartData[i].id);
-    });
+  const btnRemove = removeButton[i];
+  btnRemove.addEventListener('click', () => {
+    remove(cartData[i].id);
+  });
 }
 
 function update(i) {
-    const increment = document.querySelectorAll("#increment");
+  const increment = document.querySelectorAll('#increment');
 
-    const decrement = document.querySelectorAll("#decrement");
+  const decrement = document.querySelectorAll('#decrement');
 
-    let totalPrice = 0;
+  let totalPrice = 0;
 
-    for (let i = 0; i < decrement.length; i++) {
-        const button1 = decrement[i];
-        const button2 = increment[i];
-        button1.addEventListener("click", () => {
-            console.log("clicked");
-            const val = document.getElementById(`form1${i}`).value;
-            console.log(typeof Number(val));
-            document.getElementById(`total${i}`).innerText =
-                cartData[i].price * Number(val);
-            updateTotal();
-        });
+  for (let i = 0; i < decrement.length; i++) {
+    const button1 = decrement[i];
+    const button2 = increment[i];
+    button1.addEventListener('click', () => {
+      console.log('clicked');
+      const val = document.getElementById(`form1${i}`).value;
+      console.log(typeof Number(val));
+      //document.getElementById(`total${i}`).innerText = document.getElementById('media1').innerText;
+      document.getElementById(`total${i}`).innerText = cartData[i].price * Number(val);
+      updateTotal();
+    });
 
-        button2.addEventListener("click", () => {
-            // console.log('clicked');
-            const val = document.getElementById(`form1${i}`).value;
-            console.log(typeof Number(val));
-            document.getElementById(`total${i}`).innerText =
-                cartData[i].price * Number(val);
-            updateTotal();
-        });
-    }
+    button2.addEventListener('click', () => {
+      // console.log('clicked');
+      const val = document.getElementById(`form1${i}`).value;
+      console.log(typeof Number(val));
+      document.getElementById(`total${i}`).innerText = cartData[i].price * Number(val);
+      updateTotal();
+    });
+  }
 }
 
 function updateTotal() {
-    cartData = JSON.parse(localStorage.getItem("cart")) || [];
-    var amt = 0;
+  cartData = JSON.parse(localStorage.getItem('cart')) || [];
+  var amt = 0;
 
-    for (let j = 0; j < cartData.length; j++) {
-        let temp = Number(document.getElementById(`total${j}`).innerText);
-        console.log(typeof temp);
-        amt += temp;
-        console.log(document.getElementById(`total${j}`).innerText);
-    }
+  for (let j = 0; j < cartData.length; j++) {
+    let temp = Number(document.getElementById(`total${j}`).innerText);
+    console.log(typeof temp);
+    amt += temp;
+    console.log(document.getElementById(`total${j}`).innerText);
+  }
 
-    totalPriceSpan.innerText = amt;
-        sessionStorage.setItem('cartAmount',amt);
-
+  totalPriceSpan.innerText = amt;
+  sessionStorage.setItem('cartAmount', amt);
 }
--- payment.js
+++ payment.js
@@ -7,20 +7,21 @@ const pay = document.getElementById('pay');
 const temp = upi.innerHTML;
 const payAmt = sessionStorage.getItem('cartAmount');
 const payment = document.getElementsByClassName('payment');
+const count = document.querySelector(".badge");
 
-pay.innerHTML=`&#8377;${payAmt}` ;
-pay.disabled =true;
+pay.innerHTML = `&#8377;${payAmt}`;
+pay.disabled = true;
 
 let done = false;
 
-online.addEventListener('click',()=>{
-    upi.innerHTML=temp;
+online.addEventListener('click', () => {
+    upi.innerHTML = temp;
     upi.style.display = 'flex';
     done = true;
 });
 
-cash.addEventListener('click',()=>{
-        upi.style.display = 'flex';
+cash.addEventListener('click', () => {
+    upi.style.display = 'flex';
     upi.style.visibility = 'visible';
     upi.innerHTML = `<h3>Thank you Be ready with cash</h3>`;
     done = true;
@@ -34,32 +35,34 @@ document.getElementById('checkBtn').onclick = (event) => {
     let tel = document.getElementById('phoneNumber');
     let add = document.getElementById('address');
     let zip = document.getElementById('zip');
-    
+
     let name = document.getElementById('cc-name');
     let cardNum = document.getElementById('cc-number');
     let expiry = document.getElementById('cc-expiration');
     let cvv = document.getElementById('cc-cvv');
 
-    if(name.value.trim()=='' ||cardNum.value.trim()=='' ||expiry.value.trim()=='' ||cvv.value.trim()==''  )
-    {
-        event.preventDefault();
-        console.log('Enter card data');
-    }
 
+    if (document.getElementById('online').checked) {
+        if (name.value.trim() == '' || cardNum.value.trim() == '' || expiry.value.trim() == '' || cvv.value.trim() == '') {
+            event.preventDefault();
+            upi.style.color = 'red';
+            console.log('Enter card data');
+        }
+    }
 
     if (fName.value.trim() === '') {
         fName.value = "First Name cannot be empty";
         fName.style.color = 'red';
         reset(fName);
         event.preventDefault();
-    } 
+    }
 
     if (lName.value.trim() === '') {
         lName.value = "Last Name cannot be empty";
         lName.style.color = 'red';
         reset(lName);
         event.preventDefault();
-    } 
+    }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.com$/;
 
@@ -68,21 +71,21 @@ document.getElementById('checkBtn').onclick = (event) => {
         email.style.color = 'red';
         reset(email);
         event.preventDefault();
-    } 
+    }
 
     const phoneRegex = /^[1-9]\d{9}$/;
-    if (phoneRegex.test(tel.value.trim())== false) {
+    if (phoneRegex.test(tel.value.trim()) == false) {
         tel.style.color = 'red';
         reset(tel);
         event.preventDefault();
-    } 
+    }
 
     if (add.value.trim() === '') {
         add.value = "Address cannot be empty";
         add.style.color = 'red';
         reset(add);
         event.preventDefault();
-    } 
+    }
 
     const zipRegex = /^\d{6}$/;
     if (!zipRegex.test(zip.value.trim())) {
@@ -90,20 +93,23 @@ document.getElementById('checkBtn').onclick = (event) => {
         zip.style.color = 'red';
         reset(zip);
         event.preventDefault();
-    } 
-    if(done==false)
-    {
+    }
+    if (done == false) {
         console.log("Choose payment method");
         event.preventDefault();
     }
 
-    function reset(val){
-        val.onclick= ()=>
-        {
-                        val.style.color = 'black';
-                        val.value='';
+    function reset(val) {
+        val.onclick = () => {
+            val.style.color = 'black';
+            val.value = '';
         }
-        
+
     }
 }
 
+document.querySelector("#my_cart").addEventListener("click", () => {
+  window.location.href = "cart.html";
+});
+const cartData = JSON.parse(localStorage.getItem("cart")) || [];
+    count.innerHTML = cartData.length;
